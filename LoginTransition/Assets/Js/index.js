2c00773de2ba697ec86fadc8fe5dfda85bcf0fa8
Wender Natanael <wendertestes01@gmail.com>
2023-11-16 19:51:15 -0300

    dark theme

--- LoginTransition/Assets/Js/index.js
+++ LoginTransition/Assets/Js/index.js
@@ -1,9 +1,12 @@
-const container_signIn = document.getElementById('container_signIn')
-const background_orange = document.getElementById('background_orange')
-const container_background_orange = document.getElementById('container_background_orange')
-const container_CreatAcoonout = document.getElementById('container_CreatAcoonout')
-let model_SignIn = true
-
+const container_signIn = document.getElementById('container_signIn') //? Section sign in
+const background_orange = document.getElementById('background_orange') //? background laranja
+const container_background_orange = document.getElementById('container_background_orange') //? container dentro do background laranja
+const container_CreatAcoonout = document.getElementById('container_CreatAcoonout') //? Section create acconunt
+const container_btn_theme = document.getElementById('container_btn_theme') //? container btn tema
+const container_icon_theme = document.getElementById('container_icon_theme') //? btn tema
+const icon_theme = document.getElementById('icon_theme') //? img tema
+let model_SignIn = true //? Vai identificar se o user quer fazer login ou cadastrar
+let themeLight = true //? Vai identificar se o tema é claro ou escuro
 
 function changeModel() {
 
@@ -34,12 +37,81 @@ function changeModel() {
     model_SignIn = !model_SignIn
 }
 
-// Adicione a lógica para rolar para a direita
+//? Adicione a lógica para rolar para a direita
 function scrollRight() {
     container_background_orange.style.left = '-100%'
 }
 
-// Adicione a lógica para rolar para a esquerda
+//? Adicione a lógica para rolar para a esquerda
 function scrollLeft() {
     container_background_orange.style.left = '0%'
+}
+
+container_btn_theme.addEventListener('click', () => {
+    clickChangeTheme()
+})
+
+function clickChangeTheme() {
+    if(themeLight) {
+        themeLight = false
+        container_icon_theme.className = 'Dark_Active'
+        setTimeout(() => {
+            // icon_theme.src = '/Assets/Imgs/moon.png'
+        }, 300)
+        changeTheme(darkMod)
+
+    } else {
+        themeLight = true
+        container_icon_theme.className = 'Light_Active'
+        setTimeout(() => {
+            // icon_theme.src = '/Assets/Imgs/night-mode.png'
+        }, 300)
+        changeTheme(linghtMod)
+    }
+}
+
+window.onload = getThemeFromLocalStorage
+
+const darkMod = {
+    '--black': '#fff',
+    '--white': '#292929',
+}
+
+const linghtMod = {
+    '--black': '#000',
+    '--white': '#fff',
+}
+
+function changeTheme(theme) {
+    for (let [proeperty, value] of Object.entries(theme)) {
+        changeProperty(proeperty, value)
+    }
+
+    saveThemeToLocalStorage(theme)
+}
+
+function changeProperty(proeperty, value) {
+    const rootElement = document.documentElement //? Vai pegar o root
+    rootElement.style.setProperty(proeperty, value)
+}
+
+function saveThemeToLocalStorage(theme) {
+    localStorage.setItem('theme', JSON.stringify(theme))
+}
+
+function getThemeFromLocalStorage() {
+    const theme = JSON.parse(localStorage.getItem('theme'))
+    changeTheme(theme)
+    if(isThemeEqual(theme, darkMod)) clickChangeTheme()
+
+    setTimeout(() => {
+        addTransitionTheme()
+    }, 300)
+}
+
+function isThemeEqual(firstTheme, secondTheme) {
+    for(let prop in firstTheme) {
+        if(firstTheme[prop] !== secondTheme[prop]) return false;
+    }
+    return true
 }
